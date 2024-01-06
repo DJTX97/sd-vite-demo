95d6142ec46bd1a363c3b990ba3909637eacd94c
DJTX97 <the.10.of.one@gmail.com>
2024-01-06 20:53:27 +0200

    feat: added prompt validation

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-yellow-300 via-purple-300 to-green-400">
       <Header />
-      <div className="flex flex-col-reverse items-center md:flex-row gap-10 md:justify-evenly py-8">
+      <div className="flex flex-col-reverse items-center md:flex-row gap-10 md:justify-evenly pb-8">
         <div className="flex flex-col items-center gap-10 md:gap-5">
           <Input />
           <GenerateBtn />
--- src/components/GenerateBtn.tsx
+++ src/components/GenerateBtn.tsx
@@ -22,23 +22,30 @@ export default function GenerateBtn() {
   const [prompt, setPrompt] = useAtom(input);
 
   const [, setImage] = useAtom(output);
-  const [, setIsLoading] = useAtom(loading);
-  const handleClick = async () => {
+  const [isLoading, setIsLoading] = useAtom(loading);
+  const handleGenerate = async () => {
     setIsLoading(true);
 
     setPrompt((prev) => (prev === prompt ? prompt + " " : prompt)); //add spaces to prompt based if previous input matches the current one
 
-    const response = await query({
-      inputs: prompt,
-    });
+    //console.log(prompt)
 
-    if (response.type === "application/json") {
-      const result = JSON.parse(await response.text());
-      console.log(result);
+    if (!prompt.trim()) {
       setIsLoading(false);
+      alert("Please enter a prompt");
     } else {
-      //console.log(response)
-      setImage(response);
+      const response = await query({
+        inputs: prompt,
+      });
+
+      if (response.type === "application/json") {
+        const result = JSON.parse(await response.text());
+        console.error(result);
+        setIsLoading(false);
+      } else {
+        //console.log(response)
+        setImage(response);
+      }
     }
   };
 
@@ -49,8 +56,11 @@ export default function GenerateBtn() {
 
   return (
     <button
-      onClick={handleClick}
-      className="w-1/2 bg-blue-700 hover:bg-blue-500 p-5 rounded text-2xl text-white font-bold"
+      onClick={handleGenerate}
+      className={`w-1/2 bg-blue-700 ${
+        !isLoading ? "hover:bg-blue-500" : "opacity-60 cursor-not-allowed"
+      } p-5 rounded text-2xl text-white font-bold`}
+      disabled={isLoading}
     >
       Generate
     </button>
