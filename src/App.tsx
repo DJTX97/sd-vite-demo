bab8e2c0eec7822600292003b5bd8363b021f138
DJTX97 <the.10.of.one@gmail.com>
2024-01-06 12:25:27 +0200

    feat: gradient background

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import Output from "./components/Output";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-green-400">
+    <div className="min-h-screen bg-gradient-to-br from-yellow-300 via-purple-300 to-green-400">
       <div className="mb-10 pt-10 text-3xl font-bold text-center">
         Stable Diffusion Demo
       </div>
       <div className="flex flex-col-reverse items-center md:flex-row gap-10 md:justify-evenly">
-        <div className="flex flex-col items-center gap-5 align">
+        <div className="flex flex-col items-center gap-5">
           <Input />
           <GenerateBtn />
         </div>
